d32857236a576cf1c9c45ea060a6619e39e076ac
imjh8 <imjh8@JHLIM>
2023-04-08 14:47:35 +0900

    first

---
+++ src/main/java/com/betwe/eurekaserver/SpringBasic1Application.java
@@ -0,0 +1,13 @@
+package com.betwe.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasic1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/betwe/eurekaserver/SpringBasic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betwe.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasic1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
